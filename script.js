b6ca3c04ca5302f5e893d94c967182cdc519c64b
RodrigoRedivo <rodrigoredivo@rodrigoredivo>
2021-01-19 22:49:21 -0300

    Project Finalized! :rocket:

---
+++ script.js
@@ -0,0 +1,10 @@
+let playlist = document.querySelector('.playlist');
+let options = document.querySelector('.options');
+
+function open_p() {
+    playlist.classList.toggle('active');
+}
+
+function sidebar() {
+    options.classList.toggle('active2');
+}
